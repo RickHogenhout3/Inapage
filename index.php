de1b23a4f6a0afd208319864e3d8216c28573b3b
@rickhogenhout <2385-rickhogenhout@users.noreply.bitlab.bit-academy.nl>
2023-06-01 10:19:04 +0200

    front page

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>INAZUMA ELEVEN: Victory Road</title>
+    <style>
+        header {
+            background: #000;
+            color: #fff;
+            height: 48px;
+        }
+
+        .background-image {
+            background-image: url("img/set/bg_visual_pc.jpg");
+            /* Additional CSS properties for the background */
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 660px;
+        }
+        body {
+            background-color: #000;
+        }
+        .movie-image {
+            background-image: url("img/set/bg_movie_pc.jpg");
+            /* Additional CSS properties for the background */
+            background-repeat: no-repeat;
+            background-size: cover;
+            height: 420px;
+        }
+        .story-image{
+            background-image: url("img/set/bg_story_pc.jpg");
+        }
+    </style>
+</head>
+<body>
+<header>
+    <img src="img/set/img_logo_l5.png" style="width:250px;height:25px;position:absolute;left:7%;top:2%" alt="l5">
+    <div style="position: absolute; left:80%; top:2%;">
+        <a href="https://www.inazuma.jp/victory-road/en/"><img src="img/set/btn_twitter.png" alt="tweet" style="width:24px;height:24px;"></a>
+        |
+        <img src="img/set/icn_language.png" alt="world" style="height:24px">
+        Nederlands
+    </div>
+</header>
+<p class="background-image">
+    <img src="img/set/img_logo.png" alt="Victory Road" style="position: absolute; left:11%">
+    <h3 style="position:absolute; left:40%; top:10%">A brand new Inazuma Eleven game, returning to its roots!</h3>
+    <p class="visual" style="position:absolute; left:66.5%; top:15%">Play through Story Mode with a new protagonist,</p>
+    <p class="visual" style="position:absolute; left:56%; top:18%">and meet all characters from the series' long history in Chronicle Mode!</p>
+</p>
+
+<p class="movie-image">
+    <img src="img/set/img_movie_pv_vision.png" alt="trailer" class="trailer" style="position: absolute; left:10%; top:100%;">
+    <img src="img/set/img_bnr_blog.png" alt="blog" style="position: absolute; left:60%; top:98%">
+    <img src="img/set/img_bnr_mappa.png" alt="mappa" style="position: absolute; left:60%; top:126%">
+</p>
+<img src="img/set/tit_select_mode_pc.png" alt="modes" style="position: absolute; left:3%; top:150%;">
+<p class="story-image"></p>
+</body>
+</html>
+</html>
