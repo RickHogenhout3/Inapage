5531a0ba715e451b7e1c8a31b35e857a8ce5e987
@rickhogenhout <2385-rickhogenhout@users.noreply.bitlab.bit-academy.nl>
2023-06-23 15:13:16 +0200

    going carousel

--- meet.php
+++ meet.php
@@ -9,98 +9,122 @@
     <link rel="icon" type="image-x-icon" href="img/set/img_logo.png">
     <title>INAZUMA ELEVEN: Victory Road</title>
 </head>
+<?php include_once "header.php";?>
 <body style="background-color: #000;">
-    <?php include 'header.php';?>
+<div id="characterCarousel" class="carousel slide" data-ride="carousel" style="position:absolute; top:20%">
+    <ol class="carousel-indicators">
+        <li data-target="#characterCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#characterCarousel" data-slide-to="1"></li>
+        <li data-target="#characterCarousel" data-slide-to="2"></li>
+        <li data-target="#characterCarousel" data-slide-to="3"></li>
+        <li data-target="#characterCarousel" data-slide-to="4"></li>
+        <li data-target="#characterCarousel" data-slide-to="5"></li>
+        <li data-target="#characterCarousel" data-slide-to="6"></li>
+        <li data-target="#characterCarousel" data-slide-to="7"></li>
+        <li data-target="#characterCarousel" data-slide-to="8"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="character-container">
+                <div class="mark-evans">
+                <img class="character-image" src="img/characters/Mark_Evans.webp" alt="Mark Evans">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Mark Evans</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">He is the passionate captain of the Raimon soccer club, who never gives up no matter what the situation. He is a soccer fool, but he is a friend and is very trusted by those around him.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <div class="axel-blaze">
+                <img class="character-image" src="img/characters/Axel_Blaze.webp" alt="Axel Blaze">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Axel Blaze</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">The “Flame Ace Striker” whose name is known to everyone. He supported Raimon Junior High in the previous tournament as their trump card. The shoot he makes is like explosive fire.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Jude_Sharp.webp" alt="Jude Sharp">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Jude Sharp</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">Otherwise known as "The Absolute Leader of the Pitch," he is the commander who leads his team to victory with precise orders. He came from Raimon to Polestar Academy as a reinforcement committee member.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Arion_Sherwind.png" alt="Arion Sherwind">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Arion Sherwind</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">A cheerful boy who loves soccer. He has a refreshing personality that’s similar to a gentle breeze.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Riccardo_DiRigo.png" alt="Riccardo Di Rigo">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Riccardo Di Rigo</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">Raimon's new captain has earned the name "The Tactician of the Gods".</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Victor_Blade.png" alt="Victor Blade">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Victor Blade</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">A dark side striker who overpowers those around him with his prominent talents.</p>
+                </div>
+            </div>
+        </div>
 
-    <div class="mark-evans" style="position:absolute; top:15%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Mark_Evans.webp" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Mark Evans</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">He is the passionate captain of the Raimon soccer club, who never gives up no matter what the situation. He is a soccer fool, but he is a friend and is very trusted by those around him.</p>
+                <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Sonny_Wright.png" alt="Sonny Wright">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Sonny Wright</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">His straightforward and passionate attitude toward soccer engages those around him and leads the team to victory. He was entrusted with the captaincy because of his talent.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Elliot_Ember.png" alt="Elliot Ember">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Elliot Ember</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">Ember was feared by those around him as a Devil of the Field, but with Jude's guidance, he grew as a striker. Although Elliot's mannerisms are bad, the two have a strong trust relationship.</p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="character-container">
+                <img class="character-image" src="img/characters/Heath_Moore.png" alt="Heath Moore">
+                <div class="character-description">
+                    <h2 style="color: yellow; text-shadow: 0 0 5px blue;">Heath Moore</h2>
+                    <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">He is an all-round player who is a forward and commands all versions of the game. He is calm and collected, and overwhelms the opposing team with his sharp, anticipatory tactics.</p>
+                </div>
+            </div>
         </div>
     </div>
     </div>
-    <div class="axel-blaze" style="position: absolute; top:100%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Axel_Blaze.webp" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Axel Blaze</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">The “Flame Ace Striker” whose name is known to everyone. He supported Raimon Junior High in the previous tournament as their trump card. The shoot he makes is like explosive fire.</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="jude-sharp" style="position: absolute; top:175%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Jude_Sharp.webp" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Jude Sharp</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">Otherwise known as "The Absolute Leader of the Pitch," he is the commander who leads his team to victory with precise orders. He came from Raimon to Polestar Academy as a reinforcement committee member.</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="arion-sherwind" style="position: absolute; top:245%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Arion_Sherwind.png" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Arion Sherwind</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">A cheerful boy who loves soccer. He has a refreshing personality that’s similar to a gentle breeze.</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="riccardo-dirigo" style="position: absolute; top:305%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Riccardo_DiRigo.png" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Riccardo Di Rigo</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">Raimon's new captain has earned the name "The Tactician of the Gods".</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="victor-blade" style="position: absolute; top:360%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Victor_Blade.png" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Victor Blade</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">A dark side striker who overpowers those around him with his prominent talents.</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="sonny-wright" style="position:absolute; top:435%;">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Sonny_Wright.png" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Sonny Wright</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">His straightforward and passionate attitude toward soccer engages those around him and leads the team to victory. He was entrusted with the captaincy because of his talent.</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="elliot-ember" style="position: absolute; top:490%">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Elliot_Ember.png" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Elliot Ember</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">Ember was feared by those around him as a Devil of the Field, but with Jude's guidance, he grew as a striker. Although Elliot's mannerisms are bad, the 2 have a strong trust relationship.</p>
-        </div>
-    </div>
-    </div>
-
-    <div class="heath-moore" style="position: absolute; top:545%;">
-    <div class="character-container">
-        <img class="character-image" src="img/characters/Heath_Moore.png" alt="Character Image">
-        <div class="character-description">
-            <h2 style="color:yellow; text-shadow: 0 0 5px blue;">Heath Moore</h2>
-            <p style="color: lightcyan; text-shadow: 2px 2px #000; font: bold 20px/1.5 Helvetica, Verdana, sans-serif;">He is an all-round player who is a forward and commands all versions of the game. He is calm and collected, and overwhelms the opposing team with his sharp, anticipatory tactics.</p>
-        </div>
-    </div>
-    </div>
-
-    <?php include 'footer.php';?>
+    <a class="carousel-control-prev" href="#characterCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#characterCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <?php include_once "footer.php";?>
 </body>
 </html>
