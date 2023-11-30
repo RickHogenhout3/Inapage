65b4321c83a14d8256754c7557db55993bb081c1
@rickhogenhout <2385-rickhogenhout@users.noreply.bitlab.bit-academy.nl>
2023-11-30 15:27:23 +0100

    avatars & stickers

---
+++ inalink/chat.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <link rel="icon" type="image-x-icon" href="img/Screenshot_2023-11-15_124317-removebg-preview.png">
+    <title>Inalink</title>
+</head>
+<body background="img/inalink.png" style="background-size: cover; background-attachment: fixed;">
+    <?php include 'header.php' ?> <br>
+    <section class="form signup container">
+        <a href=""><i class="fas fa-arrow-left"></i></a>
+        <header class="d-flex">
+            <img class="chat-profilepic" src="avatars/Someoka_Ryugo_adult_avatar.png" alt="">
+            <div class="details">
+                <span style="font-weight: bold;">Someoka Ryugo</span>
+                <p>active now</p>
+            </div>
+    </section>
+</body>
+</html>
